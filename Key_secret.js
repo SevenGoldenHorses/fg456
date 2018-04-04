badb3c07c643840ded688d57600643993fc0c6ac
MiterX <you@example.com>
2018-04-04 20:33:12 +0200

    Make it better

---
+++ Key_secret.js
@@ -0,0 +1,7 @@
+module.exports = {
+  consumer_key:         'PUQCz2cmoSRhkfhctLQtOPaKX',
+  consumer_secret:      '3whaHnuFSHHYuDmkMQYfqVm7bwtMt25qIBTqoBrJ35kpEkM7s6',
+  access_token:         '902450991311851520-4MLmVobzsl0gdKuBGd1ofFZ7wKsc2Bx',
+  access_token_secret:  'VHRnh64j4pTthuJmdBnrUK6fjPtYTsi1G7lcQkCIKmUBh',
+  // timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+}
